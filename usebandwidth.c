a4d1027689a84f4c0482f74e3e4deb13c93d68d8
Andy Cranston <andy.cranston@commscope.com>
2024-11-24 14:37:33 +0000

    Basic command line parsiing done

--- usebandwidth.c
+++ usebandwidth.c
@@ -28,6 +28,8 @@
 
 #define PORTBASE 55555
 
+#define MIN_PACKET_SIZE 10
+
 #define MAX_IPv4_ADDRESS_LENGTH 15
 
 #define FAILSAFE_MAX 256
@@ -81,36 +83,111 @@ int main(argc, argv)
   int   argc;
   char *argv[];
 {
-  char     *ipv4_address;
-  int       dest_port_number;
-  int       count;
-  int       sock;
-  struct    sockaddr_in   destaddress;
-  socklen_t addresssize;
-  char     *basenetwork;
-  int       startsubnet;
-  int       endsubnet;
-  char      ipaddress[MAX_IPv4_ADDRESS_LENGTH+sizeof(char)];
-  int       i;
-  int       failsafecounter;
-  int       messagetextlength;
-  int       bytes_sent;
+	char		*ipv4_address;
+	int		dest_port_number;
+	int		count;
+	int		packet_size;
+	int		arg;
+	unsigned char	*packet;
+	int       sock;
+	struct    sockaddr_in   destaddress;
+	socklen_t addresssize;
+	char     *basenetwork;
+	int       startsubnet;
+	int       endsubnet;
+	char      ipaddress[MAX_IPv4_ADDRESS_LENGTH+sizeof(char)];
+	int       i;
+	int       failsafecounter;
+	int       messagetextlength;
+	int       bytes_sent;
 
-  progname = basename(argv[0]);
+	progname = basename(argv[0]);
 
-  if (argc < 3) {
-    usage();
-    exit(1);
-  }
+	ipv4_address = NULL;
+	dest_port_number = 55555;
+	count = 10;
+	packet_size = 1000;
 
-  ipv4_address = argv[1];
-  dest_port_number = atoi(argv[2]);
+	arg = 1;
 
-  count = 10;
+	while (arg < argc) {
+		if (strcmp(argv[arg], "-i") == 0) {
+			arg++;
 
-  if (argc > 3) {
-    count = atoi(argv[3]);
-  }
+			if (arg >= argc) {
+				fprintf(stderr, "%s: expected IPv4 destination address after -i command line option\n", progname);
+				exit(2);
+			}
+
+			ipv4_address = argv[arg];
+		} else if (strcmp(argv[arg], "-p") == 0) {
+			arg++;
+
+			if (arg >= argc) {
+				fprintf(stderr, "%s: expected UDP port number after -p command line option\n", progname);
+				exit(2);
+			}
+
+			dest_port_number = atoi(argv[arg]);
+		} else if (strcmp(argv[arg], "-c") == 0) {
+			arg++;
+
+			if (arg >= argc) {
+				fprintf(stderr, "%s: expected packet count after -c command line option\n", progname);
+				exit(2);
+			}
+
+			count = atoi(argv[arg]);
+		} else if (strcmp(argv[arg], "-s") == 0) {
+			arg++;
+
+			if (arg >= argc) {
+				fprintf(stderr, "%s: expected packet size after -s command line option\n", progname);
+				exit(2);
+			}
+
+			packet_size = atoi(argv[arg]);
+		} else {
+			fprintf(stderr, "%s: unrecognised command line argument \"%s\"\n", progname, argv[arg]);
+			exit(2);
+		}
+
+		arg++;
+	}
+
+	if (ipv4_address == NULL) {
+		fprintf(stderr, "%s: must specify a destination IPv4 address with the -i command line option\n", progname);
+		exit(2);
+	}
+
+	if (packet_size < MIN_PACKET_SIZE) {
+		fprintf(stderr, "%s: packet size of %d specified with command line option is less than minimum allowed value of %d\n", progname, packet_size, MIN_PACKET_SIZE);
+		exit(2);
+	}
+
+	if ((packet = malloc((size_t)packet_size)) == NULL) {
+		fprintf(stderr, "%s: unable to allocate a packet buffer of %d bytes\n", progname, packet_size);
+		exit(2);
+	}
+
+	for (i = 0; i < packet_size; i++) {
+		if (i < MIN_PACKET_SIZE) {
+			packet[i] = '0';
+		} else {
+			packet[i] = (char)('A' + (i % 26));
+		}
+	}
+
+	for (i = 0; i < packet_size; i++) {
+		putchar(packet[i]);
+	}
+	putchar('\n');
+
+	exit(0);
+
+
+
+
 
   printf("IPv4=%s   Port=%d   Count=%d\n", ipv4_address, dest_port_number, count);
 
